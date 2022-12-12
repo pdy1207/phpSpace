516fe60df238266c60bf323e96ccc055f4753b68
DoYoung Park <cym7356@naver.com>
2022-12-12 16:30:07 +0900

    connect.php DB경로 변경 / 메모장 참고

---
+++ 비밀공간/list.php
@@ -0,0 +1,175 @@
+<?php 
+  include 'inc_head.php';
+  include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>V비즈링 관리자</title>
+    <link rel="stylesheet" href="./index.css" />
+    <link rel="stylesheet" href="./spin.css" />
+
+<?php
+
+
+    if(isset($_POST['search'])){
+        $catagory = $_POST['catgo'];
+        $search_con = $_POST['search'];
+        $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from admin.tb_bizring where $catagory like '%$search_con%' and biz_type = '간편' order by biz_regtime desc";
+    } else {
+        $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from admin.tb_bizring where biz_type = '간편' order by biz_regtime desc;";
+    }
+
+    $sql = ""
+?>
+    <script>
+    function download() {
+        location.href = "download.php";
+    }
+
+    function regist() {
+        alert('테스트 중입니다.');
+    }
+
+    function hold() {
+        alert('테스트 중입니다.');
+    }
+
+    function reload() {
+        location.href = "./list.php";
+    }
+
+    </script>
+
+</head>
+
+<body>
+<?php
+        if ( $jb_login ) {        
+
+/*
+      echo "rckey 값: ".$_SESSION['rckey']."<br/>";
+
+*/
+
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+    echo "서버 연결 실패!";
+}
+
+// $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from ssfire4u.tb_bizring where biz_type = '간편' order by biz_regtime desc;";
+
+// echo $sql;
+
+$result = $connect->query($sql);
+
+$total_rows = mysqli_num_rows($result);
+
+    ?>
+
+    <div class=top style = "margin : 10px">
+        <h2>목록  total : <?php echo "$total_rows"; ?> 명</h2>        
+    </div>
+    <div style = "margin : 10px">
+        <button id="logout" onclick="location.href ='logout.php';">로그아웃</button>
+        <button id="exel_down" onclick='download()'>엑셀 다운로드</button>        
+        <button id="exel_down" onclick='regist()'>선택 등록완료하기</button>
+        <button id="exel_down" onclick='hold()'>선택 보류하기</button>
+        <button id="exel_down" onclick='reload()'>전체보기</button>
+    </div>
+    <div style = "height: 30px; margin : 10px">
+        <form action="list.php" method="post">
+            <select name="catgo" style = "padding:0px">
+                <option value="biz_name">이름</option>
+                <option value="biz_rc">RC번호</option>
+                <option value="biz_phone">휴대폰</option>
+                <option value="biz_birth">생년월일</option>
+            </select>
+            
+            <input type="text" name="search" size="20" />
+<!--
+            <input type = "date" name ="date_from" id="fromDate"  />
+            <input type = "date" name ="date_to" id="toDate" />
+
+            <script>
+                
+                var today = new Date().toISOString().substring(0, 10);
+                var thisday = new Date();
+                var tomorrow = new Date(thisday.setDate(thisday.getDate() + 1));
+                
+                document.getElementById('fromDate').value = today;
+                document.getElementById('toDate').value = tomorrow.toISOString().substring(0, 10);
+                
+            </script>
+-->
+            <button>검색</button>
+
+            <!-- 등록 시간 기준으로 찾기 -->
+            <form action="list.php" method="post"> 
+            <label for="date">날짜를 선택하세요 : </label>
+            <input type="Date" name="search_date"  />
+            <button>검색</button>
+        </form>
+
+
+
+    </div>
+    <table class=middle>
+        <thead>
+            <tr align=center>
+                <th width=60>타잎</th>
+                <th width=120>이름</th>
+                <th width=100>RC번호</th>
+                <th width=60>통신사</th>
+                <th width=100>휴대폰</th>
+                <th width=80>생년월일</th>
+                <th width=60>성별</th>
+                <th width=160>등록시간</th>
+                <th width=80>상태</th>
+            </tr>
+        </thead>
+        <?php
+
+
+    while($row = $result->fetch_array()){
+
+        ?>
+
+        <tbody>
+            <tr align=center>
+                <td><?php echo $row['biz_type'];?></td>
+                <td><?php echo $row['biz_name'];?></td>
+                <td><?php echo $row['biz_rc'];?></td>
+                <td><?php if ($row['biz_phonecorp'] == 'KTF'){$phonecorp = 'KT';} else { $phonecorp = $row['biz_phonecorp'];} echo $phonecorp;?></td>
+                <td><?php echo $row['biz_phone'];?></td>
+                <td><?php echo $row['biz_birth'];?></td>
+                <td><?php echo mb_substr($row['biz_gender'],0,1,'UTF-8');?></td>
+                <td><?php echo $row['biz_regtime'];?></td>
+                <td><?php echo $row['biz_flag'];?></td>
+            </tr>
+        </tbody>
+
+        <?php } ?>
+    </table>
+
+<?php
+        $connect->close();
+
+    } else {
+        echo "<script>
+        alert('로그인하십시오.');
+        </script>";
+        echo "<script>
+        document.location.href = 'index.php';
+        </script>";
+    }
+    
+?>
+</body>
+
+</html>
---
+++ 비밀공간/list2.php
@@ -0,0 +1,210 @@
+<?php 
+  include 'inc_head.php';
+  include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>V비즈링 관리자</title>
+    <link rel="stylesheet" href="./index.css" />
+    <link rel="stylesheet" href="./spin.css" />
+
+    <?php
+
+    if(isset($_POST['search'])){
+        $catagory = $_POST['catgo'];
+        $search_con = $_POST['search'];
+        $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from admin.tb_bizring where $catagory like '%$search_con%' and biz_type = '간편' order by biz_regtime desc";
+    } else {
+        $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from admin.tb_bizring where biz_type = '간편' order by biz_regtime desc;";
+    }
+
+?>
+
+    <script type="text/javascript">
+    function revcheck(theform) {
+        for (var i = 0; i < document.myform.url_num.length; i++) {
+            var ele = document.myform.url_num[i];
+            if (ele.name == 'url_num')
+                ele.checked = !ele.checked;
+        }
+        return;
+    }
+
+    //체크된 목록 삭제
+    function del() {
+        j = 0;
+        var cnt = 0;
+        var a = new Array();
+        var chk = document.forms.myform;
+
+        for (i = 0; i < chk.length; i++) {
+            if (chk[i].checked) {
+                j = 1;
+                a[cnt] = chk[i].value;
+                cnt = cnt + 1;
+            }
+        } //end of for
+
+        if (j == 0) {
+            alert("목록을 체크하여 주십시요")
+            return false;
+        } else {
+            ans = confirm("체크목록을 '삭제' 합니다.")
+            if (ans == true) {
+                //return true;
+                var acnt = a;
+                document.myform.action = "delete_ok.php?chk=" + acnt;
+                document.myform.target = "_self";
+                document.myform.submit();
+            } else {
+                return false;
+            } // end of ans
+        } // end of if
+    }
+
+
+    function download() {
+        location.href = "download.php";
+    }
+
+    function regist() {
+        alert('테스트 중입니다.');
+    }
+
+    function hold() {
+        alert('테스트 중입니다.');
+    }
+
+    function reload() {
+        location.href = "./list.php";
+    }
+    </script>
+
+</head>
+
+<body>
+    <?php
+        if ( $jb_login ) {        
+
+/*
+      echo "rckey 값: ".$_SESSION['rckey']."<br/>";
+
+*/
+
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+    echo "서버 연결 실패!";
+}
+
+// $sql = "select IFNULL(biz_type, '0') as biz_type, IFNULL(biz_name, '0') as biz_name, IFNULL(biz_rc, '0') as biz_rc, IFNULL(biz_phonecorp, '0') as biz_phonecorp, IFNULL(biz_phone, '0') as biz_phone, IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag from ssfire4u.tb_bizring where biz_type = '간편' order by biz_regtime desc;";
+
+// echo $sql;
+
+$result = $connect->query($sql);
+
+$total_rows = mysqli_num_rows($result);
+
+    ?>
+
+    <div class=top style="margin : 10px">
+        <h2>목록 total : <?php echo "$total_rows"; ?> 명</h2>
+    </div>
+    <div style="margin : 10px">
+        <button id="logout" onclick="location.href ='logout.php';">로그아웃</button>
+        <button id="exel_down" onclick='download()'>엑셀 다운로드</button>
+        <button id="exel_down" onclick='regist()'>선택 등록완료하기</button>
+        <button id="exel_down" onclick='hold()'>선택 보류하기</button>
+        <button id="exel_down" onclick='reload()'>전체보기</button>
+    </div>
+    <div style="height: 30px; margin : 10px">
+        <form action="list.php" method="post">
+            <select name="catgo" style="padding:0px">
+                <option value="biz_name">이름</option>
+                <option value="biz_rc">RC번호</option>
+                <option value="biz_phone">휴대폰</option>
+                <option value="biz_birth">생년월일</option>
+            </select>
+
+            <input type="text" name="search" size="20" />
+            <!--
+            <input type = "date" name ="date_from" id="fromDate"  />
+            <input type = "date" name ="date_to" id="toDate" />
+
+            <script>
+                
+                var today = new Date().toISOString().substring(0, 10);
+                var thisday = new Date();
+                var tomorrow = new Date(thisday.setDate(thisday.getDate() + 1));
+                
+                document.getElementById('fromDate').value = today;
+                document.getElementById('toDate').value = tomorrow.toISOString().substring(0, 10);
+                
+            </script>
+-->
+            <button>검색</button>
+        </form>
+    </div>
+    <table class=middle>
+        <thead>
+            <tr align=center>
+                <th width=60>타잎</th>
+                <th width=120>이름</th>
+                <th width=100>RC번호</th>
+                <th width=60>통신사</th>
+                <th width=100>휴대폰</th>
+                <th width=80>생년월일</th>
+                <th width=60>성별</th>
+                <th width=160>등록시간</th>
+                <th width=80>상태</th>
+
+                <th width=80><a href='javascript:revcheck(this.form)'>전체 선택</a></th>
+            </tr>
+        </thead>
+        <?php
+
+
+    while($row = $result->fetch_array()){
+
+        ?>
+
+        <tbody>
+            <tr align=center>
+                <td><?php echo $row['biz_type'];?></td>
+                <td><?php echo $row['biz_name'];?></td>
+                <td><?php echo $row['biz_rc'];?></td>
+                <td><?php if ($row['biz_phonecorp'] == 'KTF'){$phonecorp = 'KT';} else { $phonecorp = $row['biz_phonecorp'];} echo $phonecorp;?>
+                </td>
+                <td><?php echo $row['biz_phone'];?></td>
+                <td><?php echo $row['biz_birth'];?></td>
+                <td><?php echo mb_substr($row['biz_gender'],0,1,'UTF-8');?></td>
+                <td><?php echo $row['biz_regtime'];?></td>
+                <td><?php echo $row['biz_flag'];?></td>
+                <td><input type="checkbox" name="url_num" value="<?=$list_row['no']?>"></td>
+            </tr>
+        </tbody>
+
+        <?php } ?>
+    </table>
+
+    <?php
+        $connect->close();
+
+    } else {
+        echo "<script>
+        alert('로그인하십시오.');
+        </script>";
+        echo "<script>
+        document.location.href = 'index.php';
+        </script>";
+    }
+    
+?>
+</body>
+
+</html>
