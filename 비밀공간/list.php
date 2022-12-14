ce151aaf50032821a39cd0e81eeaa13b7e18947c
DoYoung Park <cym7356@naver.com>
2022-12-14 15:58:21 +0900

    수정

--- 비밀공간/list.php
+++ 비밀공간/list.php
@@ -15,13 +15,11 @@
     <link rel="stylesheet" href="./spin.css" />
 
 <?php
-        if (isset($_POST['date_from'])) {
 
-            $start_date = $_POST['date_from'];
-            $end_date = $_POST['date_to'];
-
-
-            $sql = "select 
+    if(isset($_POST['search'])){
+        $catagory = $_POST['catgo'];
+        $search_con = $_POST['search'];
+        $sql = "select 
                     IFNULL(biz_type, '0') as biz_type, 
                     IFNULL(biz_name, '0') as biz_name, 
                     IFNULL(biz_rc, '0') as biz_rc, 
@@ -31,18 +29,12 @@
                     IFNULL(biz_gender, '0') as biz_gender, 
                     IFNULL(biz_regtime, '0') as biz_regtime, 
                     IFNULL(biz_flag, '0') as biz_flag 
-            from admin.tb_bizring where $start_date like '%$end_date' and biz_type = '간편' between %$start_date% and %$end_date%";
-
-        }
-        // isset()의 함수 isset( $var1, $var2) 등 설정되어있는지 확인함. 설정되어있음 true, 그렇지 않다면 false
-        // HTML의 form을 이용하여 값을 전송하는 방식은 get post 허나 get 같은경우는 URL에 남음 ,
-        // post 는 URL에 안남음 POST방식으로 전송된 값은 $_POST 으로 받습니다.
-    if(isset($_POST['search'])){ 
-        $catagory = $_POST['catgo'];
-        $search_con = $_POST['search'];
+                from admin.tb_bizring where $catagory like '%$search_con%' and biz_type = '간편' 
+                order by biz_regtime desc";
+    } else {
         $sql = "select 
                     IFNULL(biz_type, '0') as biz_type, 
-                    IFNULL(biz_name, '0') as biz_name,                     
+                    IFNULL(biz_name, '0') as biz_name, 
                     IFNULL(biz_rc, '0') as biz_rc, 
                     IFNULL(biz_phonecorp, '0') as biz_phonecorp, 
                     IFNULL(biz_phone, '0') as biz_phone, 
@@ -50,17 +42,8 @@
                     IFNULL(biz_gender, '0') as biz_gender, 
                     IFNULL(biz_regtime, '0') as biz_regtime, 
                     IFNULL(biz_flag, '0') as biz_flag 
-                from admin.tb_bizring where $catagory like '%$search_con%' and biz_type = '간편' order by biz_regtime desc";
-    } else {
-        $sql = "select 
-        IFNULL(biz_type, '0') as biz_type, 
-        IFNULL(biz_name, '0') as biz_name, 
-        IFNULL(biz_rc, '0') as biz_rc, 
-        IFNULL(biz_phonecorp, '0') as biz_phonecorp, 
-        IFNULL(biz_phone, '0') as biz_phone, 
-        IFNULL(biz_birth, '0') as biz_birth, IFNULL(biz_gender, '0') as biz_gender, 
-        IFNULL(biz_regtime, '0') as biz_regtime, IFNULL(biz_flag, '0') as biz_flag 
-    from admin.tb_bizring where biz_type = '간편' order by biz_regtime desc;";
+                from admin.tb_bizring where biz_type = '간편' 
+                order by biz_regtime desc;";
     }
 
 ?>
@@ -119,36 +102,32 @@ $total_rows = mysqli_num_rows($result);
         <button id="exel_down" onclick='hold()'>선택 보류하기</button>
         <button id="exel_down" onclick='reload()'>전체보기</button>
     </div>
-    <div style = "height: 30px; margin : 10px">
-        <form action="list.php" method="post">
-            <select name="catgo" style = "padding:0px">
-                <option value="biz_name">이름</option>
-                <option value="biz_rc">RC번호</option>
-                <option value="biz_phone">휴대폰</option>
-                <option value="biz_birth">생년월일</option>
-            </select>
-            
-            <input type="text" name="search" size="20" />
+    <div style="display: flex;">
+            <div style = "height: 30px; margin : 10px; ">
+                <form action="list.php" method="post">
+                    <select name="catgo" style = "padding:0px">
+                        <option value="biz_name">이름</option>
+                        <option value="biz_rc">RC번호</option>
+                        <option value="biz_phone">휴대폰</option>
+                        <option value="biz_birth">생년월일</option>
+                    </select>
+                    
+                    <input type="text" name="search" size="20" />
+                    
+                    <button>검색</button>
+                </form>
+            </div>
 
+        <div style = "height: 30px; margin : 15px 10px; ">
+            <form action="datelist.php" method="POST">           
+                <input type = "date" name ="date_to"  />
+                <input type = "date" name ="date_from" style="margin-right: 10px;" />
+                <button type="submit">날짜 검색</button>        
+            </form>         
+        </div>
 
-            
-            <input type = "date" name ="date_from" id="fromDate"  />
-            <input type = "date" name ="date_to" id="toDate" />
-           
-            <script>
-                
-                var today = new Date().toISOString().substring(0, 10);
-                var thisday = new Date();
-                var tomorrow = new Date(thisday.setDate(thisday.getDate() + 1));
-                
-                document.getElementById('fromDate').value = today;
-                document.getElementById('toDate').value = tomorrow.toISOString().substring(0, 10);
-                
-            </script>
-
-            <button>검색</button>
-        </form>
     </div>
+
     <table class=middle>
         <thead>
             <tr align=center>
@@ -203,5 +182,3 @@ $total_rows = mysqli_num_rows($result);
 </body>
 
 </html>
-
-- https://hanuscrypto.tistory.com/entry/Php%EB%A1%9C-%EC%9B%B9-%EA%B0%9C%EB%B0%9C%ED%95%98%EA%B8%B0-%EA%B2%8C%EC%8B%9C%ED%8C%90-%EB%A7%8C%EB%93%A4%EA%B8%B016-%EB%82%A0%EC%A7%9C-%EA%B8%B0%EA%B0%84-%EC%A1%B0%ED%9A%8C
