63d0622a35598ba4b911bdf2ad4084a902d046de
DoYoung Park <cym7356@naver.com>
2022-12-19 14:41:31 +0900

    Update 한글깨짐.sh

--- 공간/한글깨짐.sh
+++ 공간/한글깨짐.sh
@@ -1,1 +1,11 @@
+<head>안에 utf-8로 세팅합니다.
+
+<meta charset="utf-8" /> 
+
+길게 쓰면
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+php 한글
+
 <?php header("Content-Type:text/html;charset=utf-8"); ?>
